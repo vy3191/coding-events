933333218c3c1ec160343eb397a4913368179487
Chris Bay <chris@chrisbay.net>
2022-02-14 12:46:54 -0600

    Initial commit - create and view events

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventsController.java
@@ -0,0 +1,46 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Chris Bay
+ */
+@Controller
+@RequestMapping(value = "events")
+public class EventsController {
+
+    private static ArrayList<String> events = new ArrayList<>();
+
+    // Lives at /events
+    @GetMapping("")
+    public String displayAllEvents(Model model) {
+
+        model.addAttribute("events", events);
+        model.addAttribute("title", "All Events");
+
+        return "events/index";
+    }
+
+    // lives at /events/create
+    @GetMapping("create")
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create New Event");
+        return "events/create";
+    }
+
+    // lives at /events/create
+    @PostMapping("create")
+    public String processCreateEventForm(@RequestParam String event) {
+        events.add(event);
+        return "redirect:";
+    }
+
+
+}
