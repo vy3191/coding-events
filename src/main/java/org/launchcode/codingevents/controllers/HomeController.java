c846af215623fd8e51433ab33a75a20ccad5fa47
Chris Bay <chris@chrisbay.net>
2022-02-15 10:48:06 -0600

    Adds boostrap

--- src/main/java/org/launchcode/codingevents/controllers/HomeController.java
+++ src/main/java/org/launchcode/codingevents/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("title", "Coding Events");
         return "index";
     }
 
